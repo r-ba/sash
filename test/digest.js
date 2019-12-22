90deda6b0e8042f8509ccadb241de2b53c3021d5
r-ba <rhysbalevicius@gmail.com>
2019-12-22 18:48:35 -0500

    Update mocha to ^6.2.2

--- test/digest.js
+++ test/digest.js
@@ -14,9 +14,9 @@ describe('Digested hashes', function(){
     expect( s.digest() ).to.equal("0x68E51C247FACDFC71E1AC8A5D5AC0276A4C1F86E6530BB57BEBC5CF527506858");
   });
 
-  it('File: "../binding.gyp"', function() {
+  it('File: "binding.gyp"', function() {
     const s = sash.create();
-    fs.createReadStream("../binding.gyp", { encoding: 'utf8' }).on('data', chunk => {
+    fs.createReadStream("binding.gyp", { encoding: 'utf8' }).on('data', chunk => {
       s.update(chunk);
     }).on('end', () => {
       expect(s.digest()).to.equal("0x3ECDAA34ECD2F4213FD587FC671EE6C832CEF9A430CB9C6B3344B3178D5BFD62");
