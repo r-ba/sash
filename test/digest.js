3c86bb2ccb50dceff72510c8ed419bea8a693fb3
r-ba <rhysbalevicius@gmail.com>
2019-12-22 22:38:33 -0500

    Extend tests

--- test/digest.js
+++ test/digest.js
@@ -2,25 +2,52 @@ const fs = require('fs');
 const expect = require('chai').expect;
 const sash = require('../index.js');
 
-describe('Digested hashes', function(){
+describe('create()', function(){
 
-  it('Empty initialization', function() {
+  it('Empty initialization: create()', function() {
     const s = sash.create();
-    expect( s.digest() ).to.equal("0x75CC611BB69638C08FC7DFE3044C9ADAAFB943CB1E2EE4ACFDBDB5B1406862B2");
+    expect(s.digest()).to.equal("0x75CC611BB69638C08FC7DFE3044C9ADAAFB943CB1E2EE4ACFDBDB5B1406862B2");
   });
 
-  it('String: "hello world!"', function() {
+  it('String "hello world!": create()', function() {
     const s = sash.create("hello world!");
-    expect( s.digest() ).to.equal("0x68E51C247FACDFC71E1AC8A5D5AC0276A4C1F86E6530BB57BEBC5CF527506858");
+    expect(s.digest()).to.equal("0x68E51C247FACDFC71E1AC8A5D5AC0276A4C1F86E6530BB57BEBC5CF527506858");
   });
 
-  it('File: "binding.gyp"', function() {
+  it('File "binding.gyp": create()', function() {
+    const s = sash.create();
+    let fileContent;
+    fs.createReadStream("binding.gyp", { encoding: 'utf8' }).on('data', chunk => {
+      fileContent += chunk;
+    }).on('end', () => {
+      s.update(fileContent);
+      expect(s.digest()).to.equal("0x87C01EFC2C70482D632EC32011719A66B6FE4A2122D610D819F884BD2FADEAB0");
+    });
+  });
+
+});
+
+describe('update()', function(){
+
+  it('Empty initialization: update()', function() {
+    const s = sash.create();
+    s.update("");
+    expect(s.digest()).to.equal("0x75CC611BB69638C08FC7DFE3044C9ADAAFB943CB1E2EE4ACFDBDB5B1406862B2");
+  });
+
+  it('String "hello world!": update()', function() {
+    const s = sash.create();
+    s.update("hello world!");
+    expect(s.digest()).to.equal("0x68E51C247FACDFC71E1AC8A5D9B43056F9BB62B64CE8FDEF19986821AC383418");
+  });
+
+  it('File "binding.gyp": update()', function() {
     const s = sash.create();
     fs.createReadStream("binding.gyp", { encoding: 'utf8' }).on('data', chunk => {
       s.update(chunk);
     }).on('end', () => {
       expect(s.digest()).to.equal("0x3ECDAA34ECD2F4213FD587FC671EE6C832CEF9A430CB9C6B3344B3178D5BFD62");
     });
-  })
+  });
 
 });
